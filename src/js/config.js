f8a8e3b010fb3c38b996e912ca457915dedcf449
IvannaNinja <ivanna.heraskina@Gmail.com>
2024-01-04 18:29:37 +0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd7d25846-5fb0-4ebe-8211-687dc5121675';
+export const MODEL_CLOSE_SEC = 2.5;
